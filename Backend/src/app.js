fdc3f8f3103de94b0e2ca74dd8b7b2b5c4be9c1e
Gur07 <gurmeharsingh7@gmail.com>
2026-03-23 20:07:12 +0530

    backend cors

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -7,7 +7,7 @@ const app = express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin: "*",
+    origin: "https://resume-report.vercel.app",
     credentials: true
 }))
 
